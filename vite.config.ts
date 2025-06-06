ce36f46ab2fa705333bea5a1c271bcaf35baa562
longphan2k4 <ptlong2k4@gmail.com>
2025-06-06 12:30:11 +0700

    long-feat(auth)-login

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,20 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 5173
-  }
+    port: 5173,
+    proxy: {
+      // Cấu hình proxy cho API
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+      // Cấu hình proxy cho uploads
+      "/uploads": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
