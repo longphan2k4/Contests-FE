17b636b48d7a98f9c42ffc89524a39d38f83c845
minhdat204 <huynhminhdat1506@gmail.com>
2025-06-20 13:54:56 +0700

    Add TypeScript configuration and path mapping support

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+/// <reference types="./types/global.d.ts" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_SOCKET_URL: string;
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_TINYMCE_API_KEY: string;
+  readonly VITE_ENV: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
