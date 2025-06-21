3c89427deadfc76619714588b8f34a04cd02a2a3
LongNguyen-1209 <longnguyennhat1209@gmail.com>
2025-06-21 15:47:28 +0700

    feat(home): check conflict before pr

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,10 +3,6 @@ import { Routes, Route } from "react-router-dom";
 // import PrivateRoute from './PrivateRoute';
 import AdminRoutes from "../features/admin/AdminRoutes";
 import ContestRoutes from "../features/contest/ContestRouter";
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 import AuthRoutes from "../features/auth/routes";
 import PublicRoutes from "./PublicRoutes";
 import { AudienceOpinionPage, AUDIENCE_ROUTES } from "../features/audience";
