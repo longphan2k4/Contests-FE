c463a26fac1fa7af7fe8f58b24589ab589e47939
longphan2k4 <ptlong2k4@gmail.com>
2025-06-28 21:14:31 +0700

    longphan2k4-(auth)-middware-role-router

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,16 +1,11 @@
 import { Route } from "react-router-dom";
-import Forbidden403 from "../components/403";
-import NotFound404 from "../components/404";
 import HomePage from "../features/home/pages/HomePage";
 
 const PublicRoutes = () => {
   return (
-      <Route path="/">
-        <Route index element={<HomePage />} />
-        <Route path="403" element={<Forbidden403 />} />
-        <Route path="404" element={<NotFound404 />} />
-        <Route path="*" element={<NotFound404 />} />
-      </Route>
+    <Route path="/trang-chu">
+      <Route index element={<HomePage />} />
+    </Route>
   );
 };
 
