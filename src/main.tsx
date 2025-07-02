471ec0c34f3bc1e5d756206317ec91b4a93c1978
minhdat204 <huynhminhdat1506@gmail.com>
2025-07-02 16:15:17 +0700

    feat: Add React Query Devtools for improved debugging and performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import Notification from "./components/Notification";
 import { NotificationProvider } from "./contexts/NotificationContext";
 import { ToastProvider } from "./contexts/toastContext";
@@ -34,6 +34,7 @@ createRoot(document.getElementById("root")!).render(
           <AuthProvider>
             {" "}
             <App />
+            <ReactQueryDevtools initialIsOpen={false} />
           </AuthProvider>
         </ToastProvider>
       </NotificationProvider>
