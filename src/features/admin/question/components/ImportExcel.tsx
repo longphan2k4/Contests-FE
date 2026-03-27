7bd2779bc9fe9ab925eae7d1122c87258de6b47a
NguyenTuanKiet0306231216 <0306231216@caothang.edu.vn>
2026-03-27 08:21:12 +0700

    fix:custom ExcelQuestionExample

--- src/features/admin/question/components/ImportExcel.tsx
+++ src/features/admin/question/components/ImportExcel.tsx
@@ -60,48 +60,54 @@ export default function ImportExcelDialog({
   };
 
   const handleExportExcel = () => {//sua
-    const topic_arr=topics.map((t,i)=>{
-        return{  
-        STT: i+2,
+    const topic_arr = topics.map((t, i) => {
+      return {
+        STT: i + 2,
         "Giới thiệu": "",
         "Thời gian mặc định (>10s) ": "",
         "Loại câu hỏi": "",
         "Nội dung câu hỏi": "",
-        "Lựa chọn":"",
-        "Đáp án đúng":"",
-        "Điểm":"",
-        "Độ khó(Alpha|Beta|Rc|Gold)":"",
-        "Giải thích":"",
-        "Chủ đề":"",
-        "Trạng thái":"",
-        "loaicau":"",//loai cau tu generate
-        "chude":"",//chu de tu generate
-        "question topic id":t.id,
-        "Tên chủ Đề":t.name,
-        "question type":"",
-        "loại câu hỏi":"",
+        "Lựa chọn": "",
+        "Đáp án đúng": "",
+        "Điểm": "",
+        "Độ khó(Alpha|Beta|Rc|Gold)": "",
+        "Giải thích": "",
+        "Chủ đề": "",
+        "Trạng thái": "",
+        "question_type": "",//loai cau tu generate
+        "topic_id": "",//chu de tu generate
+        "question topic id": t.id,
+        "Tên chủ Đề": t.name,
+        "question type": i == 0 ? "multiple_choice"
+          : i === 1
+            ? "essay"
+            : "",
+        "loại câu hỏi": i == 0 ? "Trắc nghiệm"
+          : i === 1
+            ? "Tự luận"
+            : "",
       }
-      })
+    })
     const data: any = [
-       {
+      {
         STT: 1,
         "Giới thiệu": "Câu hỏi Toán học nâng cao",
         "Thời gian mặc định (>10s) ": 10,
-        "Loại câu hỏi": ``,
+        "Loại câu hỏi": `Trắc nghiệm`,
         "Nội dung câu hỏi": "Đạo hàm của hàm số y = x² là?",
-        "Lựa chọn":"x | 2x | x² | 2x²",
-        "Đáp án đúng":"2x",
-        "Điểm":"20",
-        "Độ khó(Alpha|Beta|Rc|Gold)":"Alpha",
-        "Giải thích":"Đạo hàm của x² là 2x",
-        "Chủ đề":"3",
-        "Trạng thái":"Hoạt động",
-        "loaicau":"",
-        "chude":"",
-        "question topic id":"",
-        "Tên chủ Đề":"",
-        "question type":"",
-        "loại câu hỏi":"",
+        "Lựa chọn": "x|2x|x²|2x²",
+        "Đáp án đúng": "2x",
+        "Điểm": "20",
+        "Độ khó(Alpha|Beta|Rc|Gold)": "Alpha",
+        "Giải thích": "Đạo hàm của x² là 2x",
+        "Chủ đề": "Khoa học xã hội",
+        "Trạng thái": "Hoạt động",
+        "question_type": `=XLOOKUP(D2,$R$3:$R$4,$Q$3:$Q$4,"")`,
+        "topic_id": `=XLOOKUP(K2,$P$3:$P$4,$O$3:$O$4,"")`,
+        "question topic id": "",
+        "Tên chủ Đề": "",
+        "question type": "",
+        "loại câu hỏi": "",
       },
       ...topic_arr
     ];
