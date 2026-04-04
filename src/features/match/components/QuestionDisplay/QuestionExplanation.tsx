4c90fce700e2cf5091d99a08ffc5727b43bf4116
tnnq <0306231146@caothang.edu.vn>
2026-04-04 12:44:11 +0700

    fix(control): question display when click play button

--- src/features/match/components/QuestionDisplay/QuestionExplanation.tsx
+++ src/features/match/components/QuestionDisplay/QuestionExplanation.tsx
@@ -23,13 +23,13 @@ const QuestionExplanation: React.FC<QuestionExplanationProp> = ({
     <div className="min-h-[55vh] w-full p-6 bg-white rounded-lg shadow-md border transition-all duration-300 break-words overflow-auto">
       
       {/* Title */}
-      <h1 className="font-semibold mb-4 text-[clamp(14px,2.3vw,36px)]">
+      <h1 className="font-semibold text-[clamp(14px,2.3vw,36px)]">
         Mở rộng thêm về câu hỏi:
       </h1>
 
       {/* Content */}
       <div
-        className="leading-relaxed text-[clamp(14px,2.3vw,35px)]"
+        className="text-[clamp(14px,2.3vw,35px)]"
         dangerouslySetInnerHTML={{
           __html: DOMPurify.sanitize(explanation || ""),
         }}
--- src/features/match/components/QuestionDisplay/QuestionIntro.tsx
+++ src/features/match/components/QuestionDisplay/QuestionIntro.tsx
@@ -8,7 +8,7 @@ const QuestionIntro: React.FC<QuestionIntroProp> = ({ intro }) => {
   return (
     <div className="min-h-[50vh] p-6 bg-white rounded-lg shadow-md border">
     {/* quy: kích thước chữ */}
-      <h1 className="text-[clamp(14px,2.5vw,34px)] font-bold tracking-tight text-slate-900">Thông tin câu hỏi</h1>
+      <h1 className="text-[clamp(14px,2.3vw,34px)] font-bold tracking-tight text-slate-900">Thông tin câu hỏi</h1>
       <div
         className="text-[clamp(14px,2.3vw,34px)] font-medium leading-relaxed text-slate-800 antialiased"
       >
